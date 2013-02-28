a0dec583498a78727ab5f4c4a3f80409369d47fe
Simon Tang <swtSimonWingTang@gmail.com>
2013-02-27 22:10:52 -0500

    Started project.

---
+++ SwtNoSQL/Azure/AzureTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwtLib.Azure
+{
+    public class AzureTable : NoSQLTable
+    {
+        public static bool Exist(AzureNoSQLDatabase database, string tableName)
+        {
+            return true;
+        }
+
+        public AzureTable(AzureNoSQLDatabase database, string name)
+        {
+        }
+
+        public void CreateTable()
+        {
+
+        }
+    }
+}
---
+++ SwtNoSQL/NoSQLAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.WindowsAzure.Storage;
+using Amazon.Auth;
+
+namespace SwtLib
+{
+    public class NoSQLAccount
+    {
+        CloudStorageAccount _azureAccount;
+
+        public NoSQLAccount()
+        {
+
+        }
+
+        public CloudStorageAccount AzureAccount
+        {
+            get { return _azureAccount; }
+        }
+    }
+}
